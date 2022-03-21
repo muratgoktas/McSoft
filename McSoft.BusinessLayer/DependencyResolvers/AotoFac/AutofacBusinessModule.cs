ac3b08162d1b3ceb6b60ad11b054a34bf854ae97
Murat GÖKTAŞ <77151500+muratgoktas@users.noreply.github.com>
2022-03-21 21:54:08 +0300

    McSoft -MuratGoktasCV Programing

---
+++ McSoft.BusinessLayer/DependencyResolvers/AotoFac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using McSoft.BusinessLayer.Abstract;
+using McSoft.BusinessLayer.Concrete;
+using McSoft.DataAccsessLayer.Abstract;
+using McSoft.DataAccsessLayer.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McSoft.BusinessLayer.DependencyResolvers.AotoFac
+{
+   public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EducationInfoManager>().As<IEducationInfoService>();
+            builder.RegisterType<EfEducationInfoDal>().As<IEducationInfoDal>();
+        }
+    }
+}
